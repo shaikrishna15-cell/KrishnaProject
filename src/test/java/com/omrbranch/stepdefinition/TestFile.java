75e9b21c9369e3ec0f77b925fb21d54a931db823
shaikrishna15-cell <shaikrishna15@gmail.com>
2026-09-19 19:30:46 +0530

    ClassA Hello Changes

--- src/test/java/com/omrbranch/stepdefinition/TestFile.java
+++ src/test/java/com/omrbranch/stepdefinition/TestFile.java
@@ -2,7 +2,7 @@ package com.omrbranch.stepdefinition;
 
 public class TestFile {
 	public void names() {
-		System.out.println("My name is krishna");
+		System.out.println("Hello from MASTER");
 	}
 
 	public void empId(int id) {
